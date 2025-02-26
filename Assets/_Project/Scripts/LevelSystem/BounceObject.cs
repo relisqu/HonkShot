926d2ad6da259cc33d2a9defda1603c214c21121
relisqu <kopkopkop47@gmail.com>
2025-02-26 23:46:54 +0300

    added punchObjectsAnimations

--- Assets/_Project/Scripts/LevelSystem/BounceObject.cs
+++ Assets/_Project/Scripts/LevelSystem/BounceObject.cs
@@ -10,5 +10,20 @@ namespace Scripts.LevelSystem
         [SerializeField] private float _bounciness;
         public float Bounciness => _bounciness;
 
+        [SerializeField] private bool _hasBounceAnimation;
+        [SerializeField] private PunchObjectAnimation _bounceAnimation;
+
+        public void OnCollisionEnter2D(Collision2D other)
+        {
+            if (!_hasBounceAnimation) return;
+
+            if (other.gameObject.TryGetComponent(out BouncingObject _))
+            {
+                if (_hasBounceAnimation)
+                {
+                    _bounceAnimation.ShowPunchAnimation();
+                }
+            }
+        }
     }
 }
---
+++ Assets/_Project/Scripts/LevelSystem/PunchObjectAnimation.cs
@@ -0,0 +1,27 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Scripts.LevelSystem
+{
+    public class PunchObjectAnimation : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer _spriteRenderer;
+        [SerializeField] private float _punchScale = 1.2f;
+        [SerializeField] private int _punchVibrato = 4;
+        [SerializeField] private Material _blinkColorMaterial;
+
+        private Tweener _animationTween;
+
+        public void ShowPunchAnimation()
+        {
+            if (_animationTween != null && _animationTween.IsPlaying()) return;
+
+            var previousMaterial = _spriteRenderer.material;
+            _spriteRenderer.material = _blinkColorMaterial;
+            _animationTween = _spriteRenderer.transform.DOPunchScale(_punchScale * Vector3.one, 0.2f, _punchVibrato, 0f).OnComplete(() =>
+            {
+                _spriteRenderer.material = previousMaterial;
+            });
+        }
+    }
+}
